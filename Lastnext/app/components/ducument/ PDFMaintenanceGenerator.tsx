5ef040cd01af90c3022a486ab534b8d36ea74797
sqreele <kkanthong@gmail.com>
2025-06-08 20:52:20 +0700

    addfxxpdfvv56

--- Lastnext/app/components/ducument/ PDFMaintenanceGenerator.tsx
+++ Lastnext/app/components/ducument/ PDFMaintenanceGenerator.tsx
@@ -1,4 +1,4 @@
-//app/components/ducument/prevertivemaintenancepdf.tsx
+//app/components/ducument/PDFMaintenanceGenerator.tsx
 import React, { useState, useEffect } from 'react';
 import { 
   PDFDownloadLink, 
